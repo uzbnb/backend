7398cf49104f040e14f2a894767bfaff4bbbeb02
excaliburrrin <136586193+excaliburrrin@users.noreply.github.com>
2024-05-06 13:32:38 +0500

    Add property management

---
+++ property/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .models import User
+
+
+# # Create your views here.
+
+# class user(APIView):
+#     model = User
+#     def get(self, request):
+#         return Response({"message":"Users returned succefully"}, {"status":200})
