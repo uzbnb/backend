e5b75fc332946b789bcbd4bfeb95306de13eb26e
abdurashidjumabayev <161049952+abdurashidjumabayev@users.noreply.github.com>
2024-05-06 16:11:16 +0500

    Add testing data

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for database..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "Database up and running successfully"
+fi
+
+python manage.py makemigrations
+python manage.py migrate
+
+exec "$@"
